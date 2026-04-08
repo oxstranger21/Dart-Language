177e531b2148fcbb1ae18c06f72b6a6eacdd3508
oxstranger21 <gopi2023@gift.edu.in>
2026-04-08 20:38:51 +0530

    add initial implementation of variable examples in code.dart

---
+++ All_Variable/code.dart
@@ -0,0 +1,54 @@
+void main() {
+
+  // var variable example
+  print('var Example');
+  var name = 'Gopi';
+  print(name); // Output: Gopi
+
+  // dynamic variable example
+  print('dynamic Example');
+  dynamic data = 'Gopi ';
+  print(data); // Output: Gopi
+
+  // Integer example 
+  print('Integer Example');
+  int age = 30;
+  print(age); // Output: 30
+
+  // double example
+  print('double Example');
+  double price = 19.99;
+  print(price); // Output: 19.99
+
+  // String example
+  print('String Example');
+  String city = 'New York';
+  print(city); // Output: New York
+
+  // boolean example
+
+  print('boolean Example');
+  bool isActive = true; 
+  print(isActive); // Output: true
+
+  // final variable example
+  print('final Example');
+  final String country = 'USA';
+  print(country); // Output: USA it will  be assigned only once and cannot be changed later
+
+  // const variable example
+  print('const Example');
+  const double pi = 3.14159;
+  print(pi); // Output: 3.14159 it is a compile-time constant and cannot be changed at runtime
+
+  // list example
+  print('List Example');
+  List<String> fruits = ['Apple', 'Banana', 'Orange'];
+  print(fruits); // Output: [Apple, Banana, Orange]
+
+  // map example
+
+  print('Map Example');
+  Map<String, int> scores = {'Alice': 90, 'Bob': 85, 'Charlie': 92};
+  print(scores); // Output: {Alice: 90, Bob: 85, Charlie  : 92}
+}
